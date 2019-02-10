b1d069ffdafa74bf5ace07b0d3bbeaa3293c7902
Mikey Ward <mw@mikey.io>
2019-02-10 00:43:32 -0500

    translate PublicEvent

---
+++ TouchTest/Fake Touches/PublicEvent.swift
@@ -0,0 +1,16 @@
+//
+//  PublicEvent.swift
+//  TouchTest
+//
+//  Created by Mikey Ward on 2/9/19.
+//  Copyright © 2019 Mikey Ward. All rights reserved.
+//
+
+import Foundation
+
+public class PublicEvent: NSObject {
+    @objc var _event: GSEventProxy? = nil
+    @objc var _timestamp: TimeInterval = 0
+    @objc var _touches: NSMutableSet? = nil
+    @objc var _keyedTouches: CFMutableDictionary? = nil
+}
