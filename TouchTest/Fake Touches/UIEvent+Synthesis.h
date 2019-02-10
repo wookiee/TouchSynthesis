6629eb35c4f601b2049770db5d59f35836983958
Mikey Ward <mw@mikey.io>
2019-02-09 22:24:53 -0500

    adds gitignore

---
+++ TouchTest/Fake Touches/UIEvent+Synthesis.h
@@ -0,0 +1,17 @@
+//
+//  GSEvent.h
+//  TouchTest
+//
+//  Created by Mikey Ward on 2/9/19.
+//  Copyright © 2019 Mikey Ward. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface GSEvent : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TouchTest/Fake Touches/UIEvent+Synthesis.m
@@ -0,0 +1,13 @@
+//
+//  GSEvent.m
+//  TouchTest
+//
+//  Created by Mikey Ward on 2/9/19.
+//  Copyright © 2019 Mikey Ward. All rights reserved.
+//
+
+#import "GSEvent.h"
+
+@implementation GSEvent
+
+@end
